734edb7dd6b94147c186f273f54c534f7598dc3d
Magali andrea Gimenez <123522629+magaligimenez@users.noreply.github.com>
2024-07-03 15:25:28 -0300

    Add files via upload

---
+++ Ejercicio_Parcial/Class1.cs
@@ -0,0 +1,43 @@
+﻿namespace Ejercicio_Parcial
+{
+    /*
+    Desarrollar una clase que incluya como atributos un string, un entero, una lista (del tipo que desee).  
+    Ademas deberán crear un método estático en dicha clase, que escriba en un archivo de texto  los datos de un objeto. 
+    El método ,ademas, debe recibir la ruta (path) del archivo. 
+    Si se pueden guardar los datos en el archivo, el método debe retornar true; de lo contrario, debe retornar false. 
+    Probar en el main la funcionalidad.
+     */
+    public class Class1
+    {
+        string nombre;
+        int edad;
+        List<string> amigos;
+
+        public Class1(string nombre, int edad, List<string> amigos)
+        {
+            this.nombre = nombre;
+            this.edad = edad;
+            this.amigos = amigos;
+        }
+
+        public static bool EscribirArchivo(Class1 obj, string path)
+        {
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(path, true))
+                {
+                    sw.WriteLine($"Nombre: {obj.nombre}");
+                    sw.WriteLine($"Edad: {obj.edad}");
+                    sw.WriteLine("Amigos: " + string.Join(", ", obj.amigos));
+                    sw.WriteLine();
+                }
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+    }
+}
---
+++ Prueba/Program.cs
@@ -0,0 +1,38 @@
+﻿using Ejercicio_Parcial;
+
+namespace Prueba
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                List<string> amigos1 = new List<string> { "lauty", "juli", "cande" };
+                Class1 obj1 = new Class1("magui", 19, amigos1);
+
+                List<string> amigos2 = new List<string> { "maite", "facu", "tomy" };
+                Class1 obj2 = new Class1("valen", 20, amigos2);
+
+                string path = "datos.txt";
+
+                if (Class1.EscribirArchivo(obj1, path))
+                {
+                    Console.WriteLine("Datos de obj1 guardados correctamente.");
+                }
+                else
+                {
+                    Console.WriteLine("Error al guardar los datos de obj1.");
+                }
+
+                if (Class1.EscribirArchivo(obj2, path))
+                {
+                    Console.WriteLine("Datos de obj2 guardados correctamente.");
+                }
+                else
+                {
+                    Console.WriteLine("Error al guardar los datos de obj2.");
+                }
+            }
+        }
+    }
+}
